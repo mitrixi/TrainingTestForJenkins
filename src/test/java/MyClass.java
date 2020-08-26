7afafc9e0592aaba07101296ebc9e8a4c1650929
Дмитрий Балалин <dbalalin@ad.local>
2020-08-26 10:56:44 +0300

    init

--- src/test/java/MyClass.java
+++ src/test/java/MyClass.java
@@ -1,9 +1,22 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
 public class MyClass {
     @Test
-    public void simpleTest() {
+    public void trueTest() {
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void falseTest() {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void openPage() {
+        open("https://jediru.net/");
     }
 }
