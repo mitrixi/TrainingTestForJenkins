f46ff1ec5fa5ee21cf3d74f2be9363fe52d2a00f
Дмитрий Балалин <dbalalin@ad.local>
2020-09-09 17:08:03 +0300

    check jenkins with usual selenium

--- src/test/java/MyClass.java
+++ src/test/java/MyClass.java
@@ -1,22 +1,41 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class MyClass {
-    @BeforeClass
-    public static void configuration() {
-//        Configuration.browser = "SelenoidDriverProvider";
-        Configuration.browser = "firefox";
-        Configuration.timeout = 999;
-        WebDriverManager.firefoxdriver().setup();
+
+    private WebDriver driver;
+//    @BeforeClass
+//    public static void configuration() {
+////        Configuration.browser = "SelenoidDriverProvider";
+//        Configuration.browser = "firefox";
+//        Configuration.timeout = 999;
+//    }
+
+    @Before
+    public void openDriver() throws MalformedURLException {
+//        final DesiredCapabilities browser = DesiredCapabilities.chrome();
+//        browser.setCapability("enableVNC", true);
+//        browser.setCapability("screenResolution", "1920x1080x24");
+//        driver = new RemoteWebDriver(new URL("http://localhost:8081/"), browser);
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+
+
     }
 
     @Test
@@ -32,7 +51,8 @@ public class MyClass {
     @Test
     public void openPage() throws InterruptedException {
         Thread.sleep(5000);
-        open("https://google.com/");
+//        open("https://google.com/");
+        driver.get("https://google.com/");
         Thread.sleep(5000);
 
 //        $x("//title[text()='Google']").shouldBe(Condition.exist);
